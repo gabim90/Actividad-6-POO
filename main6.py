1e6290a60a74de9248b1691192490653e55c64ef
gabim90 <132012551+gabim90@users.noreply.github.com>
2023-05-06 19:41:26 -0300

    Add files via upload

---
+++ main6.py
@@ -0,0 +1,26 @@
+from actividad6 import manejadorViajero
+from actividad6 import ViajeroFrecuente
+if __name__== '__main__':
+    lista = manejadorViajero()
+    lista.testViajero()
+    k = input('Ingrese numero de viajero: ')
+    n = lista.buscaViajero(k)
+    opc = None
+    if(n == None):
+        print("El numero de viajero no existe.")
+    else:
+        while(opc != '0'):
+            opc = input('''--Menu--
+            1) Consultar cantidad de millas
+            2) Acumular Millas
+            3) Canjear millas
+            0) Cerrar
+            ''')
+            if opc == '1':
+                print("La cantidad de millas es: ",lista.getMillas(n))
+            elif opc == '2':
+                nuevas_millas = int(input('Ingrese la cantidad de millas nuevas: '))
+                print("El total de millas es: ",lista.acumularMillas(nuevas_millas,n))
+            elif opc == '3':
+                cant = int(input('Ingrese cantidad de millas a canjear: '))
+                lista.canjearMillas(cant,n)
